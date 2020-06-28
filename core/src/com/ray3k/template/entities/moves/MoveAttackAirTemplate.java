9926346731490314697b5f85c8e19e55a78af332
raeleus <raymond.ray3k@gmail.com>
2020-06-28 14:50:58 -0700

    Can move in air after attack.

--- core/src/com/ray3k/template/entities/moves/MoveAttackAirTemplate.java
+++ core/src/com/ray3k/template/entities/moves/MoveAttackAirTemplate.java
@@ -8,6 +8,9 @@ import static com.ray3k.template.AnimationName.*;
 import static com.ray3k.template.entities.PerformerEntity.Mode.*;
 
 public class MoveAttackAirTemplate implements Move {
+    public final static float H_SPEED = 800f;
+    public final static float H_ACCELERATION = 2400f;
+    
     public float friction = 500f;
     public static float gravity = 2000f;
     public Animation attackAnim = GENERAL_AIR_PUNCH.animation;
@@ -42,6 +45,11 @@ public class MoveAttackAirTemplate implements Move {
         performer.gravityY = -gravity;
         if (performer.onGround) {
             performer.mode = STANDING;
+        }
+    
+        if (performer.animationState.getCurrent(0).isComplete()) {
+            if (performer.steering.left) performer.deltaX = Utils.approach(performer.deltaX, -H_SPEED, H_ACCELERATION * delta);
+            else if (performer.steering.right) performer.deltaX = Utils.approach(performer.deltaX, H_SPEED, H_ACCELERATION * delta);
         }
     }
     
